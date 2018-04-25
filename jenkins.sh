4696f34bfd177897a9a77662c34f3048d1ca3361
Kilaru <venkatakilaru10@gmail.com>
2018-04-25 15:34:42 -0400

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -4,7 +4,7 @@
 #sudo yum install epel-release
 #sudo yum update
 #sudo reboot
-sudo yum install java-1.8.0-openjdk.x86_64
+sudo yum install java-1.8.0-openjdk.x86_64 -y
 java -version
 sudo cp /etc/profile /etc/profile_backup
 echo 'export JAVA_HOME=/usr/lib/jvm/jre-1.8.0-openjdk' | sudo tee -a /etc/profile
@@ -14,10 +14,10 @@ echo $JAVA_HOME
 echo $JRE_HOME
 #install jenkins
 cd ~ 
-sudo yum install wget
+sudo yum install wget -y
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
 sudo rpm --import https://pkg.jenkins.io/redhat-stable/jenkins.io.key
-yum install jenkins
+yum install jenkins -y
 sudo systemctl start jenkins.service
 sudo systemctl enable jenkins.service
 sudo firewall-cmd --zone=public --permanent --add-port=8080/tcp
